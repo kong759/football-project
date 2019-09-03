8962ed7a4649515ebf29f2ecdf259bcdf290f1eb
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2019-09-03 12:54:00 +0900

    Add bootstrap Templates

--- football/src/main/webapp/resources/js/Templating.js
+++ football/src/main/webapp/resources/js/Templating.js
@@ -1,35 +1,102 @@
 const CONTAINER = {
-  HEADER: `<div class="container_header">
-  <div class="container_absolute_bottom">
-  <div class="logo">
-    <h1>this is logo</h1>
-  </div>
+  HEADER: `<!-- Header-->
+      <header class="header-2">
+        <!-- End headerbox-->
+        <div class="headerbox">
+          <div class="container">
+            <div class="row justify-content-between align-items-center">
+              <!-- Logo-->
+              <div class="col col-xl-2">
+                <div class="logo">
+                  <a href="/" title="Return Home">
+                    <img src="/resources/img/logo.png" alt="Logo" class="logo_img" />
+                  </a>
+                </div>
+              </div>
+              <!-- End Logo-->
 
-  <div class="navbar">
-    <a class="navbar_menu" href="/gamelist">
-      <b>게임리스트</b>
-      </a>
-     <a class="navbar_menu" href="/teamlist">
-      <b>팀리스트</b>
-      </a>
-      <a class="navbar_menu" href="/teampage">
-      <b>팀페이지</b>
-      </a>
-  </div>
-</div>
-  <div class="conatiner_header_menu">
-    <a class="header_menu" href="#">
-      <small>some menu</small>
-    </a>
-    <a class="header_menu" href="#">
-      <small>log-in</small>
-    </a>
-  </div>
-</div >`,
+              <!-- Adds nav-->
+              <div class="col col-xl-10">
+                <!-- mainmenu-->
+                <nav class="mainmenu">
+                  <div class="container">
+                    <!-- Menu-->
+                    <ul class="sf-menu" id="menu">
+                      <li class="current">
+                        <a href="/">Home</a>
+                      </li>
+                      <li class="current">
+                        <a href="/teamlist">Teams</a>
+                      </li>
+                      <li>
+                        <a href="/gamelist">Matches</a>
+                        <ul class="sub-current">
+                            <li>
+                                <a href="/gamelist">Matches</a>
+                            </li>
+                            <li>
+                                 <a href="/gameschedule">Match Calendars</a>
+                            </li>
+                        </ul>
+                      </li>
+                      <li class="current">
+                        <a href="/gameresult">Results</a>
+                      </li>
+                      <li>
+                        <a href="table-point.html">Point Table</a>
+                      </li>
+                    </ul>
+                    <!-- End Menu-->
+                  </div>
+                </nav>
+                <!-- End mainmenu-->
+               
+               
+              </div>
+              <!-- End nav Header-->
+            </div>
+          </div>
+        </div>
+        <!-- End headerbox-->
+      </header>
+      <!-- End Header-->
+      `,
   BODY: document.querySelector(".container_body"),
-  FOOTER: `<div class="container_footer">
-  <div class="footer_contents" />
-</div>`
+  FOOTER: `<!-- footer-->
+      <footer id="footer">
+        <!-- Footer Top-->
+        <div class="top-footer">
+          <!-- Logo Footer-->
+          <div class="col-lg-12">
+            <div class="logo-footer">
+              <h2>FOOTBALL&nbsp;&nbsp;MATCHING</h2>
+            </div>
+          </div>
+          <!-- End Logo Footer-->
+        </div>
+        <!-- End Footer Top-->
+
+        <!-- Links Footer-->
+        <div class="links-footer">
+          <div class="container">
+            <div class="row"></div>
+          </div>
+        </div>
+        <!-- End Links Footer-->
+      </footer>
+      <!-- End footer-->
+
+      <!-- footer Down-->
+      <div class="footer-down">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <p>&copy; 2019 Football Matching . All Rights Reserved</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- footer Down-->`
 };
 
 function initPage() {
